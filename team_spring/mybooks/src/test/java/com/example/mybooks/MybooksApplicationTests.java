8c953efc42fa2af1e07df6faae506cd6928271c1
khyunah <89136556+khyunah@users.noreply.github.com>
2022-06-08 18:04:33 +0900

    샘플 데이터

---
+++ team_spring/mybooks/src/test/java/com/example/mybooks/MybooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
