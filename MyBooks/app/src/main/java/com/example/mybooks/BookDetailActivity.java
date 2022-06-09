59624f079c2ada98d09c5a86531c3e80b6514073
devWonil <102580743+devWonil@users.noreply.github.com>
2022-06-09 11:04:39 +0900

    ,

--- MyBooks/app/src/main/java/com/example/mybooks/BookDetailActivity.java
+++ MyBooks/app/src/main/java/com/example/mybooks/BookDetailActivity.java
@@ -1,14 +1,40 @@
 package com.example.mybooks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class BookDetailActivity extends AppCompatActivity {
+
+    private ImageButton likeBtn;
+    private ImageButton likeBtnClicked;
+    private Button purchaseBtn;
+    private boolean btnCliked = false;
+    private Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
+
+        likeBtn = findViewById(R.id.likeBtn);
+        likeBtnClicked = findViewById(R.id.likeBtnClicked);
+        purchaseBtn = findViewById(R.id.purchaseBtn);
+
+        // 프래그먼트 생성
+        likeBtn.setOnClickListener(view -> {
+            fragment = new Fragment();
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+
+            transaction.replace(R.id.likeBtn, fragment);
+            transaction.commit();
+            btnCliked = true;
+        });
     }
 }
