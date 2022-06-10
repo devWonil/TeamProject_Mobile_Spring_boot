7ffa0b0197df6eb24f9eb02afa656ce89cd1085c
khyunah <89136556+khyunah@users.noreply.github.com>
2022-06-10 12:01:24 +0900

    다이어리부분

--- MyBooks/app/src/main/java/com/example/mybooks/BestSellerFragment.java
+++ MyBooks/app/src/main/java/com/example/mybooks/BestSellerFragment.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.mybooks.adapter.BestSellerAdapter;
 import com.example.mybooks.databinding.FragmentBestSellerBinding;
+
 
 import java.util.ArrayList;
 
